f7b02404ce8dfd572a99edcecf025dc58588bb70
PUGE <100284685@qq.com>
2018-09-22 16:44:16 +0800

    完善高亮显示

--- README.md
+++ README.md
@@ -1,24 +1,59 @@
-# highlighter
+# @puge/highlight
+
+
+> SVG动画效果选择框.
+![Alt text](http://github-puge.oss-cn-beijing.aliyuncs.com/highlight/high.png)
+
+## 安装 及 使用
+
+```bash
+$ npm install --save @puge/highlight
+```
+
+使用组件
+
+```vue
+<template>
+  <Highlight v-model="jsonData">
+</template>
+
+<script>
+  import Highlight from '@puge/highlight'
+  export default {
+    components: {
+      Highlight
+    },
+    data () {
+      return {
+        jsonData: {
+          "string": "12345",
+          "array": [1, 2, 3, 4]
+        }
+      }
+    }
+  }
+</script>
+```
 
 ### 参数
 
-#### value
+#### padding
 类型: `String`<br>
-必要性: `true`<br>
-默认值: `无`
+必要性: `false`<br>
+默认值: `  `
 
-json数据
+间距
 
-#### endComma
+#### spacing
+类型: `String`<br>
+必要性: `false`<br>
+默认值: `  `
+
+分个符号
+
+#### contenteditable
 类型: `Boolean`<br>
 必要性: `false`<br>
 默认值: false
 
-末尾是否添加逗号.
-
-#### isArray
-类型: `Boolean`<br>
-必要性: `false`<br>
-默认值: false
-
-是否为数组模式
+是否可被编辑
--- highlighter.vue
+++ highlighter.vue
@@ -1,5 +1,5 @@
 <template>
-  <pre contenteditable="true" class="highlighter" spellcheck ="false">{{html}}</pre>
+  <pre :contenteditable="contenteditable" class="highlighter" spellcheck ="false" v-html="html"></pre>
 </template>
 
 <script>
@@ -15,6 +15,11 @@
       spacing: {
         type: String,
         default: ' '
+      },
+      // 是否可被编辑
+      contenteditable: {
+        type: Boolean,
+        default: false
       }
     },
     data () {
@@ -27,44 +32,98 @@
     },
     methods: {
       creatElement () {
-        this.html = this.getJson(this.value, this.padding, true)
+        this.html = this.getJsonText(this.value, this.padding, true)
       },
-      getJson (json, padding, atLast) {
-        let formatted = ''
-        if (typeof json === 'string') {
-          json = JSON.parse(json)
+      getJsonText (json, padding, atLast) {
+        const type = typeof json
+        switch (type) {
+          case 'object': {
+            // 判断是数组还是对象
+            if (Array.isArray(json)) return this.parseArray(json, padding, atLast)
+            else return this.parseJson(json, padding, atLast)
+            break
+          }
+          default: {
+            const temp = JSON.parse(json)
+            return this.parseJson(temp, padding, atLast)
+          }
         }
+      },
+      parseJson (json, padding, atLast) {
+        let formatted = ''
         const keyList = Object.keys(json)
         const keyListLestIndex = keyList.length - 1
-        console.log(json)
-        formatted += '{\r\n'
+        formatted += '<span class="object-brace">{</span>\r\n'
         keyList.forEach((key, index) => {
           const value = json[key]
           const valueType = typeof value
           // 是否是最后一项
-          const atLast = index === keyListLestIndex
-          formatted += `${padding}"${key}":`
+          const atLast = (index === keyListLestIndex)
+          formatted += `${padding}<span class="property-name">"${key}"</span>:`
           switch (valueType) {
             case 'string': {
-              formatted += `${this.spacing}"${value}",\r\n`
+              if (atLast) formatted += `${this.spacing}<span class="string">"${value}</span>"\r\n`
+              else formatted += `${this.spacing}"<span class="string">${value}</span>",\r\n`
               break
             }
             case 'object': {
-              formatted += this.spacing + this.getJson(value, padding + this.padding, atLast)
+              if (Array.isArray(value)) formatted += this.spacing + this.parseArray(value, padding + this.padding, atLast)
+              else formatted += this.spacing + this.parseJson(value, padding + this.padding, atLast)
               break
             }
-            case 'boolean':
+            case 'boolean':{
+              if (atLast) formatted += `${this.spacing}<span class="boolean">${value}</span>\r\n`
+              else formatted += `${this.spacing}<span class="boolean">${value}</span>,\r\n`
+              break
+            }
             case 'number': {
-              if (atLast) formatted += `${this.spacing}${value}\r\n`
-              else formatted += `${this.spacing}${value},\r\n`
+              if (atLast) formatted += `${this.spacing}<span class="number">${value}</span>\r\n`
+              else formatted += `${this.spacing}<span class="number">${value}</span>,\r\n`
               break
             }
           }
         })
         // 加上尾部括号
         // 判断是否为最后一项以便决定是否加上逗号
-        if (atLast) formatted += `${padding.substring(2)}}\r\n`
-        else formatted += `${padding.substring(2)}},\r\n`
+        if (atLast) formatted += `${padding.substring(2)}<span class="object-brace">}</span>\r\n`
+        else formatted += `${padding.substring(2)}<span class="object-brace">}</span>,\r\n`
+        return formatted
+      },
+      parseArray (array, padding, atLast) {
+        let formatted = ''
+        const arrayLestIndex = array.length - 1
+        formatted += '<span class="array-brace">[</span>\r\n'
+        array.forEach((value, index) => {
+          const valueType = typeof value
+          // 是否是最后一项
+          const atLast = (index === arrayLestIndex)
+          switch (valueType) {
+            case 'string': {
+              if (atLast) formatted += `${padding}<span class="string">"${value}</span>"\r\n`
+              else formatted += `${padding}"<span class="string">${value}</span>",\r\n`
+              break
+            }
+            case 'object': {
+              if (Array.isArray(value)) formatted += padding + this.getJsonText(value, padding + this.padding, atLast)
+              else formatted += padding + this.getJsonText(value, padding + this.padding, atLast)
+              break
+            }
+            case 'boolean':{
+              if (atLast) formatted += `${padding}<span class="boolean">${value}</span>\r\n`
+              else formatted += `${padding}<span class="boolean">${value}</span>,\r\n`
+              break
+            }
+            case 'number': {
+              if (atLast) formatted += `${padding}<span class="number">${value}</span>\r\n`
+              else formatted += `${padding}<span class="number">${value}</span>,\r\n`
+              break
+            }
+          }
+        })
+        // 加上尾部括号
+        // 判断是否为最后一项以便决定是否加上逗号
+        if (atLast) formatted += `${padding.substring(2)}<span class="array-brace">]</span>\r\n`
+        else formatted += `${padding.substring(2)}<span class="array-brace">]</span>,\r\n`
         return formatted
       }
     },
@@ -77,4 +136,36 @@
 </script>
 
 <style>
+.highlighter {
+  width: 100%;
+  height: 100%;
+  overflow: auto;
+  display: block;
+  font-size: 12.025px;
+  line-height: 18px;
+  word-break: break-all;
+  word-wrap: break-word;
+  font-family: Menlo,Monaco,Consolas,"Courier New",monospace;
+}
+.object-brace {
+  color: #00AA00;
+  font-weight: bold;
+}
+.array-brace {
+  color: #0033FF;
+  font-weight: bold;
+}
+.property-name {
+  color: #CC0000;
+  font-weight: bold;
+}
+.string {
+  color: #007777;
+}
+.boolean {
+  color: #0000FF;
+}
+.number {
+    color: #AA00AA;
+}
 </style>
